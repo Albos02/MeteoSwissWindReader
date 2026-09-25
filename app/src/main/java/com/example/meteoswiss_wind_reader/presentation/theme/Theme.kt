b90d960d046066bdf3a8225444123f6d5250567e
Alex Bossel <alex@bossel.ch>
2026-09-26 00:23:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/meteoswiss_wind_reader/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.meteoswiss_wind_reader.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun MeteoSwissWindReaderTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
